1ffb9803be4da75f905db7c696429da426a4bd74
Julien Balestra <julien.balestra@datadoghq.com>
2020-04-17 23:56:21 +0200

    conntrack: support udp

--- pkg/collector/network/conntrack.go
+++ pkg/collector/network/conntrack.go
@@ -36,6 +36,45 @@ func (c *Conntrack) Name() string {
 	return "network/conntrack"
 }
 
+func (c *Conntrack) tcp(fields []string, tcpStats map[string]*datadog.Metric) {
+	state, srcIp, dstPort := fields[3], fields[4], fields[7]
+	srcIp = strings.TrimPrefix(srcIp, "src=")
+	dstPort = strings.TrimPrefix(dstPort, "dport=")
+	mapKey := srcIp + state + dstPort
+	st, ok := tcpStats[mapKey]
+	if !ok {
+		st = &datadog.Metric{
+			Name: "network.conntrack.tcp",
+			Host: c.conf.Host,
+			Tags: append(c.conf.Tagger.GetWithDefault(srcIp,
+				tagger.NewTag(exported.LeaseKey, tagger.MissingTagValue),
+				tagger.NewTag(selfExported.DeviceKey, tagger.MissingTagValue),
+			), "state:"+state, "src_ip:"+srcIp, "dst_port:"+dstPort),
+		}
+		tcpStats[mapKey] = st
+	}
+	st.Value++
+}
+
+func (c *Conntrack) udp(fields []string, udpStats map[string]*datadog.Metric) {
+	srcIp, dstPort := fields[3], fields[6]
+	srcIp = strings.TrimPrefix(srcIp, "src=")
+	mapKey := srcIp + dstPort
+	st, ok := udpStats[mapKey]
+	if !ok {
+		st = &datadog.Metric{
+			Name: "network.conntrack.udp",
+			Host: c.conf.Host,
+			Tags: append(c.conf.Tagger.GetWithDefault(srcIp,
+				tagger.NewTag(exported.LeaseKey, tagger.MissingTagValue),
+				tagger.NewTag(selfExported.DeviceKey, tagger.MissingTagValue),
+			), "src_ip:"+srcIp),
+		}
+		udpStats[mapKey] = st
+	}
+	st.Value++
+}
+
 func (c *Conntrack) Collect(_ context.Context) (datadog.Counter, datadog.Gauge, error) {
 	var counters datadog.Counter
 	var gauges datadog.Gauge
@@ -46,41 +85,32 @@ func (c *Conntrack) Collect(_ context.Context) (datadog.Counter, datadog.Gauge, 
 	}
 	defer file.Close()
 	reader := bufio.NewReader(file)
-	stats := make(map[string]*datadog.Metric)
-	now := time.Now()
-	hostTags := c.conf.Tagger.Get(c.conf.Host)
+	tcpStats := make(map[string]*datadog.Metric)
+	udpStats := make(map[string]*datadog.Metric)
 	for {
 		line, _, err := reader.ReadLine()
 		if err == io.EOF {
 			break
 		}
 		s := string(line)
-		if !strings.HasPrefix(s, "tcp") {
+		fields := strings.Fields(s)
+		if fields[0] == "tcp" {
+			c.tcp(fields, tcpStats)
 			continue
 		}
-		fields := strings.Fields(s)
-		state, src, dport := fields[3], fields[4], fields[7]
-		src = strings.TrimPrefix(src, "src=")
-		dport = strings.TrimPrefix(dport, "dport=")
-		mapKey := src + state + dport
-		st, ok := stats[mapKey]
-		if !ok {
-			tags := append(hostTags, c.conf.Tagger.GetWithDefault(src,
-				tagger.NewTag(exported.LeaseKey, tagger.MissingTagValue),
-				tagger.NewTag(selfExported.DeviceKey, tagger.MissingTagValue),
-			)...)
-			st = &datadog.Metric{
-				Name:      "network.conntrack.tcp",
-				Value:     0,
-				Timestamp: now,
-				Host:      c.conf.Host,
-				Tags:      append(tags, "state:"+state, "src:"+src),
-			}
-			stats[mapKey] = st
-		}
-		st.Value++
+		// udp
+		c.udp(fields, udpStats)
 	}
-	for _, st := range stats {
+	hostTags := c.conf.Tagger.Get(c.conf.Host)
+	now := time.Now()
+	for _, st := range tcpStats {
+		st.Timestamp = now
+		st.Tags = append(st.Tags, hostTags...)
+		gauges = append(gauges, st)
+	}
+	for _, st := range udpStats {
+		st.Timestamp = now
+		st.Tags = append(st.Tags, hostTags...)
 		gauges = append(gauges, st)
 	}
 	return counters, gauges, nil
