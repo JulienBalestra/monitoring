6f604203d7f9b781b1c34b780cb3b6be6eab50f0
Julien Balestra <8077192+JulienBalestra@users.noreply.github.com>
2020-04-19 15:25:26 +0200

    load: drop some metrics (#8)

--- pkg/collector/load/load.go
+++ pkg/collector/load/load.go
@@ -49,14 +49,9 @@ func (c *Load) Collect(_ context.Context) (datadog.Counter, datadog.Gauge, error
 		return counters, gauges, err
 	}
 	// TODO use scan instead ?
-	loadString := string(load[:len(load)-1])
-	parts := strings.Split(loadString, " ")
+	parts := strings.Fields(string(load))
 	if len(parts) != 5 {
-		return counters, gauges, fmt.Errorf("failed to parse %s: %q", parts, loadString)
-	}
-	subPart := strings.Split(parts[3], "/")
-	if len(subPart) != 2 {
-		return counters, gauges, fmt.Errorf("failed to parse %s: %q", subPart, parts[3])
+		return counters, gauges, fmt.Errorf("failed to parse %s: %q", parts, string(load))
 	}
 	load1, err := strconv.ParseFloat(parts[0], 10)
 	if err != nil {
@@ -70,19 +65,25 @@ func (c *Load) Collect(_ context.Context) (datadog.Counter, datadog.Gauge, error
 	if err != nil {
 		return counters, gauges, err
 	}
-	lastProcess, err := strconv.ParseFloat(parts[4], 10)
-	if err != nil {
-		return counters, gauges, err
-	}
 
-	kernelScheduling, err := strconv.ParseFloat(subPart[0], 10)
-	if err != nil {
-		return counters, gauges, err
-	}
-	kernelEntities, err := strconv.ParseFloat(subPart[1], 10)
-	if err != nil {
-		return counters, gauges, err
-	}
+	// TODO do we need these following gauges ?
+	//subPart := strings.Split(parts[3], "/")
+	//if len(subPart) != 2 {
+	//	return counters, gauges, fmt.Errorf("failed to parse %s: %q", subPart, parts[3])
+	//}
+	//lastProcess, err := strconv.ParseFloat(parts[4], 10)
+	//if err != nil {
+	//	return counters, gauges, err
+	//}
+	//
+	//kernelScheduling, err := strconv.ParseFloat(subPart[0], 10)
+	//if err != nil {
+	//	return counters, gauges, err
+	//}
+	//kernelEntities, err := strconv.ParseFloat(subPart[1], 10)
+	//if err != nil {
+	//	return counters, gauges, err
+	//}
 
 	// newMetric is a convenient way to DRY the following gauges
 	now, tags := time.Now(), c.conf.Tagger.Get(c.conf.Host)
@@ -99,8 +100,8 @@ func (c *Load) Collect(_ context.Context) (datadog.Counter, datadog.Gauge, error
 		newMetric("load.1", load1),
 		newMetric("load.5", load5),
 		newMetric("load.10", load10),
-		newMetric("process.last_id", lastProcess),
-		newMetric("kernel.scheduling", kernelScheduling),
-		newMetric("kernel.entities", kernelEntities),
+		//newMetric("process.last_id", lastProcess),
+		//newMetric("kernel.scheduling", kernelScheduling),
+		//newMetric("kernel.entities", kernelEntities),
 	), nil
 }
