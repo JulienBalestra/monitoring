5f84fedf47a0fa261c50ee919c12b6dab8625d73
Julien Balestra <8077192+JulienBalestra@users.noreply.github.com>
2020-04-20 20:02:32 +0200

    dd-wrt: split in 2 scripts (#12)

---
+++ dd-wrt/scripts/metrics.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+kill $(cat /tmp/metrics.pid)
+
+export DATADOG_API_KEY="fake-api-key********************"
+export DATADOG_HOST_TAGS=location:home,room:living-room
+
+exec /jffs/bin/metrics --pid-file=/tmp/metrics.pid \
+    --collector-datadog-client                  90s     \
+    --collector-dnsmasq                         30s     \
+    --collector-load                            15s     \
+    --collector-memory                          30s     \
+    --collector-network-arp                     30s     \
+    --collector-network-conntrack               30s     \
+    --collector-network-statistics              10s     \
+    --collector-temperature                     60s     \
+    --datadog-host-tags=${DATADOG_HOST_TAGS}
