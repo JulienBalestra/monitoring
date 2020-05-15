9373e650ff18b8ca3a5601e2ae863ce5d319a169
Julien Balestra <8077192+JulienBalestra@users.noreply.github.com>
2020-05-15 10:23:55 +0200

    conntrack: simplify (#32)

--- pkg/collector/network/conntrack.go
+++ pkg/collector/network/conntrack.go
@@ -5,16 +5,13 @@ import (
 	"strconv"
 	"time"
 
-	"go.uber.org/zap"
-
-	"github.com/JulienBalestra/monitoring/pkg/conntrack"
-
+	"github.com/JulienBalestra/monitoring/pkg/collector"
 	"github.com/JulienBalestra/monitoring/pkg/collector/dnsmasq/exported"
 	selfExported "github.com/JulienBalestra/monitoring/pkg/collector/network/exported"
-	"github.com/JulienBalestra/monitoring/pkg/tagger"
-
-	"github.com/JulienBalestra/monitoring/pkg/collector"
+	"github.com/JulienBalestra/monitoring/pkg/conntrack"
 	"github.com/JulienBalestra/monitoring/pkg/metrics"
+	"github.com/JulienBalestra/monitoring/pkg/tagger"
+	"go.uber.org/zap"
 )
 
 const (
@@ -73,15 +70,14 @@ func getPortRange(port int) string {
 }
 
 type aggregation struct {
-	toBytes              float64
-	fromBytes            float64
+	count                float64
 	protocol             string
 	destinationPortRange string
 	sourceIP             string
-	Timestamp            time.Time
+	state                string
 }
 
-func (c *Conntrack) aggregationToSamples(now time.Time, aggr *aggregation) (*metrics.Sample, *metrics.Sample) {
+func (c *Conntrack) aggregationToSamples(now time.Time, aggr *aggregation) *metrics.Sample {
 	tags := append(c.conf.Tagger.GetUnstable(c.conf.Host),
 		c.conf.Tagger.GetUnstableWithDefault(aggr.sourceIP,
 			c.tagLease,
@@ -91,29 +87,21 @@ func (c *Conntrack) aggregationToSamples(now time.Time, aggr *aggregation) (*met
 		"protocol:"+aggr.protocol,
 		"dport:"+aggr.destinationPortRange,
 		"ip:"+aggr.sourceIP,
+		"state:"+aggr.state,
 	)
 	return &metrics.Sample{
-			Name:      "network.conntrack.rx_bytes",
-			Value:     aggr.toBytes,
-			Timestamp: now,
-			Host:      c.conf.Host,
-			Tags:      tags,
-		},
-		&metrics.Sample{
-			Name:      "network.conntrack.tx_bytes",
-			Value:     aggr.fromBytes,
-			Timestamp: now,
-			Host:      c.conf.Host,
-			Tags:      tags,
-		}
+		Name:      "network.conntrack.entries",
+		Value:     aggr.count,
+		Timestamp: now,
+		Host:      c.conf.Host,
+		Tags:      tags,
+	}
 }
 
 func (c *Conntrack) Collect(ctx context.Context) error {
 	after := time.After(0)
 
-	records := make(map[uint64]*conntrack.Record)
 	aggregations := make(map[string]*aggregation)
-
 	ticker := time.NewTicker(c.conf.CollectInterval)
 	defer ticker.Stop()
 	for {
@@ -124,53 +112,24 @@ func (c *Conntrack) Collect(ctx context.Context) error {
 		case <-ticker.C:
 			now := time.Now()
 			for _, aggr := range aggregations {
-				tx, rx := c.aggregationToSamples(now, aggr)
-				_, _ = c.measures.Count(tx), c.measures.Count(rx)
+				_ = c.measures.GaugeDeviation(c.aggregationToSamples(now, aggr), c.conf.CollectInterval*3)
 			}
 			c.measures.Purge()
-			for h, record := range records {
-				if time.Since(record.Deadline) > deadlineTolerationDuration {
-					zap.L().Debug("delete record", zap.Uint64("hash", h))
-					delete(records, h)
-				}
-			}
-			for key, value := range aggregations {
-				if time.Since(value.Timestamp) > maxAgeConntrackEntries {
-					zap.L().Debug("delete aggregation",
-						zap.String("sourceIP", value.sourceIP),
-						zap.String("destinationPortRange", value.destinationPortRange),
-						zap.String("protocol", value.protocol),
-						zap.Float64("fromBytes", value.fromBytes),
-						zap.Float64("toBytes", value.toBytes),
-						zap.String("key", key),
-					)
-					delete(aggregations, key)
-				}
-			}
+			aggregations = make(map[string]*aggregation)
 
 		case <-after:
 			newRecords, closestDeadline, err := conntrack.GetConntrackRecords(conntrackPath)
 			if err != nil {
+				zap.L().Error("failed to get conntrack records", zap.Error(err))
 				continue
 			}
 			closestDeadlineIn := closestDeadline.Sub(time.Now())
-			if closestDeadlineIn < deadlineTolerationDuration {
-				closestDeadlineIn = deadlineTolerationDuration
+			if closestDeadlineIn < c.conf.CollectInterval {
+				closestDeadlineIn = c.conf.CollectInterval
 			}
 			after = time.After(closestDeadlineIn)
-			now := time.Now()
 
-			for h, newRecord := range newRecords {
-				switch newRecord.Protocol {
-				case conntrack.ProtocolTCP:
-					if newRecord.State != conntrack.StateEstablished {
-						continue
-					}
-				default:
-					if newRecord.State != conntrack.StateReplied {
-						continue
-					}
-				}
+			for _, newRecord := range newRecords {
 				portRange := getPortRange(newRecord.From.Quad.DestinationPort)
 				aKey := newRecord.Protocol + newRecord.From.Quad.Source + portRange
 
@@ -180,24 +139,11 @@ func (c *Conntrack) Collect(ctx context.Context) error {
 						protocol:             newRecord.Protocol,
 						destinationPortRange: portRange,
 						sourceIP:             newRecord.From.Quad.Source,
-						toBytes:              newRecord.To.Bytes,
-						fromBytes:            newRecord.From.Bytes,
-						Timestamp:            now,
+						state:                newRecord.State,
 					}
 					aggregations[aKey] = aggr
-					records[h] = newRecord
-					continue
 				}
-				existing, ok := records[h]
-				if ok && time.Since(existing.Deadline) < deadlineTolerationDuration {
-					aggr.toBytes += newRecord.To.Bytes - existing.To.Bytes
-					aggr.fromBytes += newRecord.From.Bytes - existing.From.Bytes
-				} else {
-					aggr.toBytes += newRecord.To.Bytes
-					aggr.fromBytes += newRecord.From.Bytes
-				}
-				aggr.Timestamp = now
-				records[h] = newRecord
+				aggr.count++
 			}
 		}
 	}
