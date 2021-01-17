2beb60de14b7618515dc100fbd0b95e356609d92
Julien Balestra <8077192+JulienBalestra@users.noreply.github.com>
2021-01-17 19:12:27 +0100

    shelly: use up.time (#91)
    * shelly: use up.time
    
    * gauge: rollback to defined

--- pkg/metrics/store.go
+++ pkg/metrics/store.go
@@ -83,9 +83,6 @@ func (st *AggregationStore) Aggregate(series ...*Series) int {
 		h := fnv.NewHash()
 		h = fnv.AddString(h, s.Metric)
 		h = fnv.AddString(h, s.Host)
-		if s.Type == TypeGauge {
-			s.Type = TypeDefaultGauge
-		}
 		h = fnv.AddString(h, s.Type)
 		h = fnv.AddString(h, strconv.FormatInt(int64(s.Interval), 10))
 
